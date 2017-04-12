ea133e39228ba28d32475fa46ca07f2d18921d20
lakshmibhargavab <lakshmibhargavab@proarch.com>
2017-04-12 12:34:29 +0530

    signup page and styling for signup page

--- app.py
+++ app.py
@@ -4,8 +4,15 @@
 from flask import Flask, render_template
 app = Flask(__name__)
 
+#Routing configuration for the Welcome page of the application
 @app.route("/")
 def main():
     return render_template('index.html')
+
+#Routing configuration for the Signup page of application
+@app.route('/showSignUp/')
+def showSignUp():
+    return render_template('signup.html')
+    
 if __name__ == "__main__":
     app.run()
